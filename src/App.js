d854db31cbb9f92dc55df5723865ad76d00e34a7
Nawed Ali <83456083+nawed2611@users.noreply.github.com>
2022-03-18 22:17:00 +0530

    Redesign UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './Components/Navbar';
-import Search from './Components/Search';
-import NewsList from './Components/NewsList';
+import SearchBar from './Components/SearchBar';
+import Home from './Pages/Home';
+import Search from './Pages/Search';
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar />
-      <Search />
-      <NewsList />
+      <SearchBar />
+
+      <Routes>
+        <Route path="/" element ={<Home />} />
+      </Routes>
+
     </div>
+    </Router>
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import "../Styles/navbar-style.css";
 
 export default function Navbar(){
 
     return(
         <div className="navbar">
-                <h2>News Centre</h2>
+                <Link to="/"><h2>News Centre</h2></Link>
                 <ul className="navlinks">
                     <a style={{color: "black"}} href="https://github.com/nawed2611/News-App" target="_blank"><li>GitHub Repo</li></a>
                 </ul>
