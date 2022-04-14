8a8c2540b23de2bfe6228f31f1c16baa6bea0823
Nawed Ali <83456083+nawed2611@users.noreply.github.com>
2022-04-15 03:53:51 +0530

    Remove form functions

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -5,16 +5,6 @@ export default function NewsItem({ title, description, url, urlToImage }) {
 
     const [value, setValue] = useState('');
 
-    const handleChange = e => {
-        setValue(e.target.value);
-        console.log(value);
-    };
-
-    const handleSubmit = e => {
-        e.preventDefault();
-        console.log(value);
-    }
-
     return (
         <div className="newsitem">
             <div className="card">
