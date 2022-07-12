5d52e0da69425fe2a453a0850612fd66de201654
Nawed Ali <nawed2611@gmail.com>
2022-07-13 01:34:35 +0530

    Add github icon

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import "../Styles/navbar-style.css";
+import { Link } from 'react-router-dom';
+import { AiFillGithub } from 'react-icons/ai';
 
-export default function Navbar(){
+export default function Navbar() {
 
-    return(
+    return (
         <div className="navbar">
-                <Link to="/"><h2>News Centre</h2></Link>
-                <ul className="navlinks">
-                    <a href="https://github.com/nawed2611/News-Centre" target="_blank" rel="noreferrer"><li>GitHub Repo</li></a>
-                </ul>
+            <Link to="/"><h2>News Centre</h2></Link>
+            <ul className="navlinks">
+                <a href="https://github.com/nawed2611/News-Centre" target="_blank" rel="noreferrer"><li><AiFillGithub size="40px" /></li></a>
+            </ul>
         </div>
 
     )
